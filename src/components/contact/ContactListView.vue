15d7c7bed3222bfa19506f06dcc947bb8e76cee5
imndx <imndxx@gmail.com>
2020-12-09 00:07:58 +0800

    update contact list

--- src/components/contact/ContactListView.vue
+++ src/components/contact/ContactListView.vue
@@ -3,9 +3,26 @@
     <ul>
       <li>
         <div @click="showNewFriends" class="category-item-container">
-          <i class="arrow right" v-bind:class="{down: expandFriend}"></i>
+          <i class="arrow right" v-bind:class="{down: expandNewFriend}"></i>
           <span class="title">新的朋友</span>
         </div>
+        <ul v-if="expandNewFriend">
+          <li v-for="(friend,index) in newFriends" :key="index">
+            <div class="new-friend-item-container" v-bind:class="{active :index ===2}">
+              <div class="new-friend-item">
+                <img class="avatar" src="@/assets/images/user-fallback.png">
+                <div class="info">
+                  <div class="name-action">
+                    <span class="name single-line">imndx</span>
+                    <span class="accepted" v-if="index >= 3">已添加</span>
+                    <button class="accept" v-if="index < 3">添加</button>
+                  </div>
+                  <p class="reason single-line">我是{{ friend }}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
       </li>
       <li>
         <div @click="showGroups" class="category-item-container">
@@ -52,7 +69,7 @@ export default {
   name: "ContactListView",
   data() {
     return {
-      expandFriend: false,
+      expandNewFriend: false,
       expandContact: false,
       expandGroup: false,
       currentGroup: 2,
@@ -64,7 +81,7 @@ export default {
   },
   methods: {
     showNewFriends() {
-      this.expandFriend = !this.expandFriend;
+      this.expandNewFriend = !this.expandNewFriend;
       // TODO
     },
     showGroups() {
@@ -116,13 +133,64 @@ export default {
 }
 
 .category-item span:last-of-type {
-  margin-right: 10px;
+  margin-right: 15px;
 }
 
 .avatar {
   width: 40px;
   height: 40px;
   border-radius: 3px;
+}
+
+.new-friend-item-container {
+  padding-left: 30px;
+}
+
+.new-friend-item {
+  display: flex;
+  width: 100%;
+  padding: 10px 15px 10px 0;
+  align-items: center;
+  font-size: 13px;
+  border-bottom: 1px solid #e0e0e0;
+}
+
+
+.new-friend-item-container.active {
+  background-color: #d6d6d6;
+}
+
+.new-friend-item-container:hover {
+  background-color: #d6d6d6;
+}
+
+.new-friend-item .info {
+  margin-left: 10px;
+  flex: 1;
+}
+
+.new-friend-item .info .name-action {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.new-friend-item .info .name-action .name {
+  flex: 1;
+}
+
+.new-friend-item .info .name-action .accept {
+  padding: 0 5px;
+  text-align: center;
+}
+
+.new-friend-item .info .name-action .accepted {
+  color: #b2b2b2;
+}
+
+.new-friend-item .info .reason {
+  font-size: 12px;
+  color: #b2b2b2;
 }
 
 .group-item {
