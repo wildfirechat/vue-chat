4f2a975a21af8ab0522ecbbd69317e0b90d66621
x86 <1960867973@qq.com>
2022-08-22 10:56:18 +0800

    优化打包速度

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,70 @@
-module.exports = {
-    devServer:{
-        disableHostCheck: true,
-    }
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir);
 }
+module.exports = {
+    devServer: {
+        disableHostCheck: true,
+        port: 8013
+    },
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    lintOnSave: process.env.NODE_ENV === 'development',
+    productionSourceMap: false,
+    configureWebpack: {
+        // provide the app's title in webpack's name field, so that
+        // it can be accessed in index.html to inject the correct title.
+        resolve: {
+            alias: {
+                '@': resolve('src')
+            }
+        }
+    },
+    chainWebpack(config) {
+        config.plugin('preload').tap(() => [
+            {
+                rel: 'preload',
+                // to ignore runtime.js
+                // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+                fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+                include: 'initial'
+            }
+        ]);
+        config.plugins.delete('prefetch');
+        config.when(process.env.NODE_ENV !== 'development', config => {
+            config.optimization.splitChunks({
+                chunks: 'all',
+                cacheGroups: {
+                    libs: {
+                        name: 'chunk-libs',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: 10,
+                        chunks: 'initial' // only package third parties that are initially dependent
+                    },
+                    commons: {
+                        name: 'chunk-commons',
+                        test: resolve('src/components'), // can customize your rules
+                        minChunks: 3, //  minimum common number
+                        priority: 5,
+                        reuseExistingChunk: true
+                    }
+                }
+            });
+            // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+            config.optimization.runtimeChunk('single');
+        });
+        config.optimization.runtimeChunk('single');
+        // 注入全局sacc变量
+        const oneOfsMap = config.module.rule('scss').oneOfs.store;
+        oneOfsMap.forEach(item => {
+            item
+                .use('sass-resources-loader')
+                .loader('sass-resources-loader')
+                .options({
+                    resources: './src/stylesheet/variables.scss'
+                })
+                .end();
+        });
+    }
+};
