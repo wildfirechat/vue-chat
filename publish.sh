92a41081137429859cb155d67e78357d76717bb2
x86 <1960867973@qq.com>
2022-12-31 10:22:39 +0800

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,11 @@
+echo 'enable ptt'
 sed -i '' 's/ENABLE_PTT = false/ENABLE_PTT = true/' src/config.js
+echo 'enable conference'
 cp src/wfc/av/internal/engine-conference.min.js src/wfc/av/internal/engine.min.js
 npm run build
+echo 'checkout modification'
 git checkout -- src/wfc/av/internal/engine.min.js
+git checkout -- src/config.js
 
 tar -czvf dist.tar.gz dist
 scp dist.tar.gz wfccn:/var/wildfirechat_sites/web
