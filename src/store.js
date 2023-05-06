eb75ac88cd96003da6968a74db24a1f800d560f6
x86 <1960867973@qq.com>
2023-05-06 23:25:37 +0800

    修复对方更新远程消息时，本地可能不能正常显示问题

--- src/store.js
+++ src/store.js
@@ -367,6 +367,7 @@ let store = {
                     return m.messageId === msg.messageId || eq(m.messageUid, msg.messageUid);
                 });
                 if (msgIndex > -1) {
+                    conversationState.currentConversationMessageList[msgIndex] = msg;
                     console.log('msg duplicate')
                     return;
                 }
