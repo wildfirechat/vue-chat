bff8e039ba22271217401a5dcf807e22962afdf1
x86 <1960867973@qq.com>
2023-07-18 09:40:33 +0800

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,12 +1,19 @@
 echo 'enable ptt'
 sed -i '' 's/ENABLE_PTT = false/ENABLE_PTT = true/' src/config.js
+sed -i '' "s|publicPath: '/',|publicPath: 'https://static.wildfirechat.cn',|" vue.config.js
 echo 'enable conference'
 cp src/wfc/av/internal/engine-conference.min.js src/wfc/av/internal/engine.min.js
 npm run build
 echo 'checkout modification'
 git checkout -- src/wfc/av/internal/engine.min.js
 git checkout -- src/config.js
+git checkout -- vue.config.js
 
+cd dist
+qshell qupload2 --src-dir=./js --bucket=firechat --key-prefix=/js/
+qshell qupload2 --src-dir=./css --bucket=firechat --key-prefix=/css/
+
+cd ..
 tar -czvf dist.tar.gz dist
 scp dist.tar.gz wfccn:/var/wildfirechat_sites/web
 ssh wfccn 'cd /var/wildfirechat_sites/web; rm -rf css/ dist favicon.ico fonts/ img/ index.html js/; tar -xzvf dist.tar.gz; mv dist/* .; rm -rf dist.tar.gz'
