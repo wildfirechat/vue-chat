3fbdb241e0bb46330e34d89d637f9feb5e4fd2cd
x86 <1960867973@qq.com>
2023-10-16 21:39:19 +0800

    更新状态码描述

--- src/wfc/client/connectionStatus.js
+++ src/wfc/client/connectionStatus.js
@@ -21,7 +21,7 @@ export default class ConnectionStatus {
             '-9': '客户端和IM 服务端时间不同步，请进行时钟同步',
             '-8': 'IM 服务未授权或已过期，专业版IM-Server 是绑定域名或者 ip 的，只能通过所绑定的域名去连接',
             '-7': '被踢下线',
-            '-6': '会话密钥错误，请参考 https://docs.wildfirechat.cn/faq/general.html 第12个问题排查',
+            '-6': '会话密钥错误，请先确认是否正确替换 proto.min.js 文件，一定要替换成邮件收到的版本!!! \n如果确认已正确替换，请参考 https://docs.wildfirechat.cn/faq/general.html 第12个问题排查',
             '-5': 'token错误',
             '-4': 'IM Server服务无法连通，请检查服务器是否宕机或者网络出现问题',
             '-3': '连接被服务器拒绝，一般是用户被封禁',
