0502de2c5d4caa62d2925ebc2bb53718f466314f
x86 <1960867973@qq.com>
2023-11-07 23:55:04 +0800

    update style

--- src/ui/main/HomePage.vue
+++ src/ui/main/HomePage.vue
@@ -439,6 +439,8 @@ i.active {
     position: absolute;
     top: 50px;
     left: 50px;
+    border-radius: 5px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     display: flex;
     flex-direction: column;
 }
