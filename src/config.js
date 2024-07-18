b2a5cf981b4561af4255fafa7e4336c1898b398d
x86 <1960867973@qq.com>
2024-07-18 11:38:43 +0800

    修改默认 turn 服务配置

--- src/config.js
+++ src/config.js
@@ -44,11 +44,12 @@ export default class Config {
     // 野火二维码 scheme，不要修改，如果需要修改的话，所有端都需要一起修改
     static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
 
-    // turn server 配置，可以添加多个
-    // !!! 我们提供的服务仅供用户测试和体验，为了保证测试可用，我们会不定期的更改密码. !!!
-    // 上线时，请一定要切换为你们自己部署的服务
-    // 格式: [uri, 用户名, 密码]，可以添加多个
-    static ICE_SERVERS = [['turn:turn.wildfirechat.net:3478', 'wfchat', 'wfchat1']];
+    // turn server 配置，可以添加多个<br>
+    // 格式: [uri, 用户名, 密码]，可以添加多个<br>
+    // Turn服务配置，用户音视频通话功能，详情参考 https://docs.wildfirechat.net/webrtc/ <br>
+    // 我们提供的服务能力有限，总体带宽仅3Mbps，只能用于用户测试和体验，为了保证测试可用，我们会不定期的更改密码。<br>
+    // 上线时请一定要切换成你们自己的服务。可以购买腾讯云或者阿里云的轻量服务器，价格很便宜，可以避免影响到您的用户体验。<br>
+    static ICE_SERVERS = [['turn:turn.wildfirechat.net:3478', 'wfchat', 'wfchat123']];
     static LANGUAGE = 'zh_CN';
 
     static MESSAGE_ROAMING = 1;
